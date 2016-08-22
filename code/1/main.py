e72682691764d03a37e935f0bcbece8553fdfe36
Ian Drosos <ian.drosos@gmail.com>
2016-08-21 20:16:40 -0400

    fixed description of assertion error

--- code/1/main.py
+++ code/1/main.py
@@ -13,7 +13,7 @@ def is_equal(x, y):
 @utest.ok
 def _okmain():
   "Adding another test case to main.py, is \"__main__\" == __name__"
-  assert is_equal("__main__", __name__), "random numbers equality failure in main test"
+  assert is_equal("__main__", __name__), "is name == main equality failure in main test"
 
 if __name__ == "__main__":
     print "\nokmain"
