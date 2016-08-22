c6d6dafb4d3046cbd38301d0f949cd3364a7bfad
Ian Drosos <ian.drosos@gmail.com>
2016-08-22 10:05:24 -0400

    adding test case to who1

--- code/1/who1.py
+++ code/1/who1.py
@@ -1,4 +1,15 @@
+import utest
 __author__ = "ian drosos"
 # python 2
 
-print "\nin who1: " + __author__
+print "\nin " + __name__ + " by " + __author__
+
+
+def is_equal(x, y):
+    return x == y
+
+    
+@utest.ok
+def _okwho1():
+  "Adding another test case to who1.py, is \"__main__\" == __name__"
+  assert is_equal("__main__", __name__), "is name == main equality failure in main test"
