4dea8d356e33a1f5565364eb6b9bf28897ee66d2
aniketdhuri <aniket.dhuri@gmail.com>
2016-08-23 11:48:58 -0400

    Added test for who2 by Aniket

--- code/1/main.py
+++ code/1/main.py
@@ -13,5 +13,15 @@ def _okmain():
   "Adding another test case to main.py, is \"__main__\" == __name__"
   assert is_equal("__main__", __name__), "is name == main equality failure in main test"
 
+@utest.ok
+def _okwho2():
+    #Testing who2.getAuthorReverse
+    assert who2.getAuthorReverse()=="iruhD tekinA"
+
+
+
 if __name__ == "__main__":
     print "\nokmain"
+
+#Final Statistics of Pass and Failure case
+utest.oks()
---
+++ code/1/who2.py
@@ -0,0 +1,7 @@
+import utest
+
+__author__="Aniket Dhuri"
+
+#Method to reverse author name
+def getAuthorReverse():
+    return __author__[::-1]
