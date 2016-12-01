ec72d0a88d8f72eabb5c64be6303b086a7c12dc1
adhuri <adhuri@ncsu.edu>
2016-11-30 23:41:50 -0500

    Fixed printStat.sh

--- project/code/printStat.sh
+++ project/code/printStat.sh
@@ -15,7 +15,16 @@ done
 
 echo $UUID
 
-for i in stats/*$UUID*.txt; do # Whitespace-safe but not recursive.
+purchase="purchase_$UUID\.txt"
+cat stats/*purchase*$UUID* >$purchase
+
+
+utility="utility_$UUID\.txt"
+cat stats/*utility*$UUID* >$utility
+
+time="time_$UUID\.txt"
+cat stats/*time*$UUID* >$time
+for i in *$UUID*.txt; do # Whitespace-safe but not recursive.
     	echo "\n\nStatistics for $i\n===================================================================================="
 	cat "$i"|python stats/stats.py
 done
